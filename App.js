9075ea0aa951ceede91288427e7cad412e245b7d
GiladLev <giladlev2000@gmail.com>
2023-01-20 11:44:11 +0200

    delete package

--- App.js
+++ App.js
@@ -6,7 +6,9 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { I18nManager } from "react-native";
 import { useContext, useEffect } from "react";
 
+I18nManager.forceRTL(false);
 
+I18nManager.allowRTL(false);
 export default function App() {
   useEffect(() => {
     I18nManager.forceRTL(false);
